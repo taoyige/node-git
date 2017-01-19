007526372954fc3b76a7bd3357f3a452148e4770
taoyige <taoyigezhong@qq.com>
2017-01-19 15:45:59 +0800

    修改了一下

--- gulpfile.js
+++ gulpfile.js
@@ -24,74 +24,74 @@ var htmlmin = require('gulp-htmlmin');
 
 
 /** 任务1 */
-gulp.task('style', function () {
-	/** src支持数字参数，在路径前面加上!表示不匹配该文件 */
-	gulp.src(['src/styles/*.less', '!src/styles/_*.less'])
-		.pipe(less())
-		.pipe(cssnano())
-		.pipe(gulp.dest('dist/styles'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('style', function() {
+    /** src支持数字参数，在路径前面加上!表示不匹配该文件 */
+    gulp.src(['src/styles/*.less', '!src/styles/_*.less'])
+        .pipe(less())
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 
 });
 
 
 /** 任务2 */
-gulp.task('script', function () {
-	/** 先合并 在混淆 */
-	gulp.src('src/scripts/*.js')
-		.pipe(concat('all.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/scripts'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('script', function() {
+    /** 先合并 在混淆 */
+    gulp.src('src/scripts/*.js')
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 })
 
 
 /** 任务3 */
-gulp.task('image', function () {
-	gulp.src('src/images/*.*')
-		.pipe(gulp.dest('dist/images'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('image', function() {
+    gulp.src('src/images/*.*')
+        .pipe(gulp.dest('dist/images'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 })
 
 
 
 /** 任务4 */
-gulp.task('html', function () {
-	gulp.src('src/*.html')
-		.pipe(htmlmin({
-			/* 去除空白字符 */
-			collapseWhitespace: true,
-			/* 去除注释 */
-			removeComments: true,
-			/* 去除引号 因为没有引号浏览器也能识别属性值，这样可以节省空间*/
-			removeAttributeQuotes: true
-		}))
-		.pipe(gulp.dest('dist/'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('html', function() {
+    gulp.src('src/*.html')
+        .pipe(htmlmin({
+            /* 去除空白字符 */
+            collapseWhitespace: true,
+            /* 去除注释 */
+            removeComments: true,
+            /* 去除引号 因为没有引号浏览器也能识别属性值，这样可以节省空间*/
+            removeAttributeQuotes: true
+        }))
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 })
 
 
 var browserSync = require('browser-sync');
-gulp.task('serve', function () {
-	browserSync({
-		server: {
-			baseDir: ['dist/'],
-		},
-		port: 2017
-	}, function(err, bs) {
-	    console.log(bs.options.getIn(["urls", "local"]));
-	});
+gulp.task('serve', function() {
+    browserSync({
+        server: {
+            baseDir: ['dist/'],
+        },
+        port: 2017
+    }, function(err, bs) {
+        console.log(bs.options.getIn(["urls", "local"]));
+    });
 
-	gulp.watch('src/styles/*.less', ['style']);
-	gulp.watch('src/scripts/*.js', ['script']);
-	gulp.watch('src/images/*.*', ['image']);
-	gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/styles/*.less', ['style']);
+    gulp.watch('src/scripts/*.js', ['script']);
+    gulp.watch('src/images/*.*', ['image']);
+    gulp.watch('src/*.html', ['html']);
 })
